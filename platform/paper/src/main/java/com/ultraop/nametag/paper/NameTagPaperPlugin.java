9df110c70f124828e33d132465816fe677240575
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-20 03:15:17 +0530

    Fix Paper service bootstrap

--- platform/paper/src/main/java/com/ultraop/nametag/paper/NameTagPaperPlugin.java
+++ platform/paper/src/main/java/com/ultraop/nametag/paper/NameTagPaperPlugin.java
@@ -1,14 +1,27 @@
 package com.ultraop.nametag.paper;
+
 import com.ultraop.nametag.api.TagService;
-import com.ultraop.nametag.common.service.DefaultTagService;
+import com.ultraop.nametag.common.DefaultTagService;
+import com.ultraop.nametag.common.InMemoryPlayerAssignmentRepository;
+import com.ultraop.nametag.common.InMemoryTagRepository;
 import com.ultraop.nametag.paper.v1_21_11.Paper2111Adapter;
 import org.bukkit.plugin.java.JavaPlugin;
+
 public final class NameTagPaperPlugin extends JavaPlugin {
     private TagService tagService;
-    @Override public void onEnable() {
-        tagService = new DefaultTagService();
+
+    @Override
+    public void onEnable() {
+        tagService = new DefaultTagService(
+                new InMemoryTagRepository(),
+                new InMemoryPlayerAssignmentRepository()
+        );
         new Paper2111Adapter(tagService);
         getLogger().info("NameTag-Core enabled.");
     }
-    @Override public void onDisable() { getLogger().info("NameTag-Core disabled."); }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("NameTag-Core disabled.");
+    }
 }
