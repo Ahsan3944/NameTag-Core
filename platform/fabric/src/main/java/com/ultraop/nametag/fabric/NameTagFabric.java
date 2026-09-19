fd8aa1704063032f6d6b7c1d2ef0f693429bc45d
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-20 03:15:14 +0530

    Fix Fabric service bootstrap

--- platform/fabric/src/main/java/com/ultraop/nametag/fabric/NameTagFabric.java
+++ platform/fabric/src/main/java/com/ultraop/nametag/fabric/NameTagFabric.java
@@ -1,11 +1,19 @@
 package com.ultraop.nametag.fabric;
+
 import com.ultraop.nametag.api.TagService;
-import com.ultraop.nametag.common.service.DefaultTagService;
+import com.ultraop.nametag.common.DefaultTagService;
+import com.ultraop.nametag.common.InMemoryPlayerAssignmentRepository;
+import com.ultraop.nametag.common.InMemoryTagRepository;
 import com.ultraop.nametag.fabric.v1_21_11.Fabric2111Adapter;
+
 public final class NameTagFabric {
     private NameTagFabric() {}
+
     public static void bootstrap() {
-        TagService service = new DefaultTagService();
+        TagService service = new DefaultTagService(
+                new InMemoryTagRepository(),
+                new InMemoryPlayerAssignmentRepository()
+        );
         new Fabric2111Adapter(service);
     }
 }
