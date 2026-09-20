7404b20a4d5cedd35a2992f1dbe1601259ae4361
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-21 00:31:24 +0530

    Wire Fabric bootstrap to YAML persistence

--- platform/fabric/src/main/java/com/ultraop/nametag/fabric/NameTagFabric.java
+++ platform/fabric/src/main/java/com/ultraop/nametag/fabric/NameTagFabric.java
@@ -2,17 +2,21 @@ package com.ultraop.nametag.fabric;
 
 import com.ultraop.nametag.api.TagService;
 import com.ultraop.nametag.common.DefaultTagService;
-import com.ultraop.nametag.common.InMemoryPlayerAssignmentRepository;
-import com.ultraop.nametag.common.InMemoryTagRepository;
+import com.ultraop.nametag.common.YamlPlayerAssignmentRepository;
+import com.ultraop.nametag.common.YamlTagRepository;
+import net.fabricmc.loader.api.FabricLoader;
 import com.ultraop.nametag.fabric.v1_21_11.Fabric2111Adapter;
 
 public final class NameTagFabric {
     private NameTagFabric() {}
 
     public static void bootstrap() {
+        java.nio.file.Path dataDirectory = FabricLoader.getInstance()
+                .getConfigDir()
+                .resolve("nametag-core");
         TagService service = new DefaultTagService(
-                new InMemoryTagRepository(),
-                new InMemoryPlayerAssignmentRepository()
+                new YamlTagRepository(dataDirectory.resolve("tags.yml")),
+                new YamlPlayerAssignmentRepository(dataDirectory.resolve("assignments.yml"))
         );
         new Fabric2111Adapter(service).registerCommands();
     }
