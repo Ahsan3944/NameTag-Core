cb174406bbdf4aef3e15b5b4a7099841bfa15c4e
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-21 22:36:48 +0530

    Add contextual resolution failure diagnostics

--- common/src/test/java/com/ultraop/nametag/common/DefaultTagServiceTest.java
+++ common/src/test/java/com/ultraop/nametag/common/DefaultTagServiceTest.java
@@ -294,7 +294,13 @@ class DefaultTagServiceTest {
         service.assign(player, new TagId("spawn"));
         service.setActive(player, new TagId("spawn"));
 
-        List<Tag> inside = service.activeTags(player, new TagResolutionContext("world_nether", 0, 64, 0));
+        List<Tag> inside;
+        try {
+            inside = service.activeTags(player, new TagResolutionContext("world_nether", 0, 64, 0));
+        } catch (RuntimeException exception) {
+            fail("Contextual resolution threw: " + exception, exception);
+            return;
+        }
         assertEquals(List.of("spawn", "world", "global"), inside.stream().map(tag -> tag.id().value()).toList());
 
         List<Tag> outside = service.activeTags(player, new TagResolutionContext("world_nether", 100, 64, 100));
