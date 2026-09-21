05349dcbb23ce09edc607e17484421ea0dede576
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-21 21:08:35 +0530

    Register automatic role command on Fabric

--- platform/fabric/src/main/java/com/ultraop/nametag/fabric/NameTagFabric.java
+++ platform/fabric/src/main/java/com/ultraop/nametag/fabric/NameTagFabric.java
@@ -145,6 +145,17 @@ public final class NameTagFabric {
                                     .executes(context -> execute(context, service, messages, configuration, new String[]{"effect", StringArgumentType.getString(context, "tag"), StringArgumentType.getString(context, "effect")})))));
 
 
+            root.then(CommandManager.literal("role")
+                    .then(CommandManager.argument("tag", StringArgumentType.word())
+                            .suggests((context, builder) -> suggestTags(context, builder, service, configuration, "role"))
+                            .then(CommandManager.argument("permission", StringArgumentType.word())
+                                    .suggests((context, builder) -> CommandSource.suggestMatching(List.of("clear"), builder))
+                                    .executes(context -> execute(context, service, messages, configuration, new String[]{
+                                            "role",
+                                            StringArgumentType.getString(context, "tag"),
+                                            StringArgumentType.getString(context, "permission")
+                                    }))));
+
             dispatcher.register(root);
         });
     }
