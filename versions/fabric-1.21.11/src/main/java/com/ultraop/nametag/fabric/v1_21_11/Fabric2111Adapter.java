a37bf40ae44c9fcdbe582bdcebfaf0975f439910
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-21 11:58:19 +0530

    Wire Fabric 1.21.11 chat integration

--- versions/fabric-1.21.11/src/main/java/com/ultraop/nametag/fabric/v1_21_11/Fabric2111Adapter.java
+++ versions/fabric-1.21.11/src/main/java/com/ultraop/nametag/fabric/v1_21_11/Fabric2111Adapter.java
@@ -1,5 +1,6 @@
 package com.ultraop.nametag.fabric.v1_21_11;
 
+import com.ultraop.nametag.api.ConfigurationService;
 import com.ultraop.nametag.api.TagService;
 import net.minecraft.server.MinecraftServer;
 
@@ -7,10 +8,14 @@ import java.util.Objects;
 
 public final class Fabric2111Adapter {
     private final Fabric2111NameplateRenderer renderer;
+    private final Fabric2111ChatRenderer chatRenderer;
 
-    public Fabric2111Adapter(TagService tagService) {
+    public Fabric2111Adapter(TagService tagService, ConfigurationService configuration) {
         Objects.requireNonNull(tagService, "tagService");
+        Objects.requireNonNull(configuration, "configuration");
         this.renderer = new Fabric2111NameplateRenderer(tagService);
+        this.chatRenderer = new Fabric2111ChatRenderer(tagService, configuration);
+        this.chatRenderer.register();
     }
 
     public void stop(MinecraftServer server) {
