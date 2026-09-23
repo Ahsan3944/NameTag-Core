0e0e691ae3b96c1832840620eb601fd456764e4f
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-23 12:34:32 +0530

    fix(fabric): disable item display interpolation

--- versions/fabric-1.21.11/src/main/java/com/ultraop/nametag/fabric/v1_21_11/mixin/DisplayEntityAccessor.java
+++ versions/fabric-1.21.11/src/main/java/com/ultraop/nametag/fabric/v1_21_11/mixin/DisplayEntityAccessor.java
@@ -1,8 +1,10 @@
 package com.ultraop.nametag.fabric.v1_21_11.mixin;
 
+import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.decoration.DisplayEntity;
 import net.minecraft.util.math.AffineTransformation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(DisplayEntity.class)
@@ -12,4 +14,15 @@ public interface DisplayEntityAccessor {
 
     @Invoker("setBillboardMode")
     void nametagCore$setBillboardMode(DisplayEntity.BillboardMode billboardMode);
+
+    @Invoker("setInterpolationDuration")
+    void nametagCore$setInterpolationDuration(int interpolationDuration);
+
+    @Invoker("setTeleportDuration")
+    void nametagCore$setTeleportDuration(int teleportDuration);
+
+    @Accessor("ITEM_DISPLAY")
+    static TrackedData<Byte> nametagCore$getItemDisplayData() {
+        throw new AssertionError();
+    }
 }
