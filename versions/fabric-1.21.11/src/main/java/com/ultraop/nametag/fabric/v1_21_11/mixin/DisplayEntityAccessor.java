c2c1eafc6a2d7b2f5cc24c8e925d0a09a9e18c59
Ahsan <80054519+Ahsan3944@users.noreply.github.com>
2026-09-23 12:41:58 +0530

    fix(fabric): target item display context on correct entity class

--- versions/fabric-1.21.11/src/main/java/com/ultraop/nametag/fabric/v1_21_11/mixin/DisplayEntityAccessor.java
+++ versions/fabric-1.21.11/src/main/java/com/ultraop/nametag/fabric/v1_21_11/mixin/DisplayEntityAccessor.java
@@ -1,10 +1,8 @@
 package com.ultraop.nametag.fabric.v1_21_11.mixin;
 
-import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.decoration.DisplayEntity;
 import net.minecraft.util.math.AffineTransformation;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(DisplayEntity.class)
@@ -20,9 +18,4 @@ public interface DisplayEntityAccessor {
 
     @Invoker("setTeleportDuration")
     void nametagCore$setTeleportDuration(int teleportDuration);
-
-    @Accessor("ITEM_DISPLAY")
-    static TrackedData<Byte> nametagCore$getItemDisplayData() {
-        throw new AssertionError();
-    }
 }
